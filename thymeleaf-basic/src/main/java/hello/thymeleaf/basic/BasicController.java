0eef54097ea4b66689dd9c2b79fd7ef17b64d19a
shimmh <31092342+shimmh@users.noreply.github.com>
2021-12-14 00:48:52 +0900

    자바스크립트 인라인

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -120,6 +120,13 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("User\"A\"", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
 
 
     public void addUsers(Model model) {
