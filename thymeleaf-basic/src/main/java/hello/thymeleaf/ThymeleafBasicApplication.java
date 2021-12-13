59da3317a0719ea6a8ed127d04bd7daa30a41006
shimmh <31092342+shimmh@users.noreply.github.com>
2021-12-13 18:04:02 +0900

    리터럴 문법

--- thymeleaf-basic/src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
@@ -1,4 +1,4 @@
-package hello.thymeleaf.basic;
+package hello.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class ThymeleafBasicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafBasicApplication.class, args);
 	}
-
 }
