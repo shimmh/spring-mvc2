4e67921e08a7882ab78db5090ac546c757381dd0
shimmh <31092342+shimmh@users.noreply.github.com>
2021-12-14 20:42:55 +0900

    상품 관리 페이지 - 체크 박스

--- form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -32,6 +32,11 @@ public class ItemRepository {
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
+        //추가 필드
+        findItem.setOpen(updateParam.getOpen());
+        findItem.setRegions(updateParam.getRegions());
+        findItem.setItemType(updateParam.getItemType());
+        findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
 
     public void clearStore() {
