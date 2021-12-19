5a01af60f388c899bc5250613692afa942774f19
shimmh <31092342+shimmh@users.noreply.github.com>
2021-12-19 18:59:31 +0900

    로그인 처리 V3 - 서블릿 HTTP 세션 적용

---
+++ login/src/main/java/hello/login/web/session/SessionConst.java
@@ -0,0 +1,8 @@
+package hello.login.web.session;
+
+//생성 할 객체가 아닌 상수만 사용하는것이기에 interface 로 선언
+public interface SessionConst {
+
+    //private static final String LOGIN_MEMBER = "loginMember";
+    String LOGIN_MEMBER = "loginMember";
+}
